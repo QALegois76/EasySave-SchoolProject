fbc7b7389b54671ac6059d5c37eb78629e9ccefc
ayonis\antoine.legois <antoine.legois@ayonis.com>
2022-01-25 09:37:30 +0100

    folderCorrection

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,11 +9,16 @@ namespace ConsoleUI
         private static CommandMng _cmdMng;
         private static JobMng _jobMng;
 
+        private static bool _maintainLoop = true;
+        private static bool _quitIsAsking = false;
+
+
         static void Main(string[] args)
         {
             _jobMng = new JobMng(new Job(""));
             ModelViewJobs modelViewJobs = new ModelViewJobs(_jobMng);
             _cmdMng = new CommandMng(modelViewJobs);
+
             _cmdMng.Start();
         }
 
---
+++ LibEasySave/Model/LogMng.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibEasySave.Model
+{
+    public class LogMng
+    {
+
+        private static LogMng instance;
+        private static readonly object lockobj = new object();
+
+        private LogMng()
+        {
+
+        }
+        public static LogMng GetInstance
+        {
+            get
+            {
+
+                if (instance == null)
+                {
+                    lock (lockobj)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new LogMng();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+
+    }
+}
