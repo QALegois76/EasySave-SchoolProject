f530dec890da4e5c26582dbeb225cf6202ddb19c
PAD <papaabdoulaye.diagne@viacesi.fr>
2022-01-27 10:02:41 +0100

    LogBaseSaver tatata

---
+++ LibEasySave/Model/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibEasySave.Model
+{
+    class Class1
+    {
+    }
+}
---
+++ LibEasySave/Model/LogBaseSaver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibEasySave.Model
+{
+    public abstract class LogBaseSaver
+    {
+        IJob _job;
+        public LogBaseSaver(IJob job)
+        {
+            _job = job;
+        }
+    }
+}
