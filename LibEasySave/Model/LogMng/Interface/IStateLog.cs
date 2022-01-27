80c2b1494505236d91f89c0b94b872203d8c9bf7
PAD <papaabdoulaye.diagne@viacesi.fr>
2022-01-27 12:15:29 +0100

    logMng done

---
+++ LibEasySave/Model/LogMng/Interface/IStateLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibEasySave.Model.LogMng.Interface
+{
+    interface IStateLog
+    {
+        public string JobName
+        {
+            get { return JobName; }
+            set { JobName = value; }
+        }
+        public DateTime Time
+        {
+            get { return Time; }
+            set { Time = value; }
+        }
+        enum EJobState
+        {
+            isRunning,
+            isDone,
+            notStarted
+        }
+    }
+}
+
