9007da12f342dca562a29e5181784e936f7faf61
ayonis\antoine.legois <antoine.legois@ayonis.com>
2022-02-08 11:27:50 +0100

    cryptoSased implemented

---
+++ CryptoSoft/CryptInfoModel/CryptFileInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace CryptoSoft
+{
+    public struct CryptFileInfo
+    {
+        private long _key;
+
+        private string _fileSrc;
+        private string _fileDest;
+
+        public long Key => _key;
+        public string FileDest => _fileDest;  
+        public string FileSrc => _fileSrc;  
+
+        public CryptFileInfo(string fileSrc, string fileDest, long key) : this()
+        {
+            this._fileSrc = fileSrc;
+            this._fileDest = fileDest;
+            this._key = key;
+        }
+
+
+    }
+}
